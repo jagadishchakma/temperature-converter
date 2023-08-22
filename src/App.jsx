c4dc9f8ccee62bdb51591856bd0bdcc151192f33
jagadishchakma <jagadishchakma401@gmail.com>
2023-08-22 20:37:04 +0600

    connection add

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import Clock from "./components/class/Clock";
+
+class App extends Component{
+ 
+  render(){
+    console.log('app')
+    return <Clock/>
+  }
+}
+
+export default App;
